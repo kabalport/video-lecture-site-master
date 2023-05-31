1f045295cadb5ebf9cddc69dd10b7f29c22b5df2
최대열 <dychoi@brainednet.com>
2023-05-31 09:37:22 +0900

    init3

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { LectureProvider } from './contexts/LectureContext';
 
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
 import Home from './pages/Home';
 import VideoLectureList from './pages/VideoLectureList';
 import VideoLectureDetail from './pages/VideoLectureDetail';
 import About from './pages/About';
-import NavBar from "./components/NavBar";
+import NavBar from "./components/layout/NavBar";
+import Career from "./pages/Career";
+import Notice from "./pages/Notice";
+import Job from "./pages/Job";
+import Faq from "./pages/Faq";
 
 const App = () => {
     return (
@@ -18,9 +22,15 @@ const App = () => {
                 <NavBar />
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/career" element={<Career />} />
+                    <Route path="/notice" element={<Notice />} />
+                    <Route path="/job" element={<Job />} />
+                    <Route path="/faq" element={<Faq />} />
+
                     <Route path="/lectures" element={<VideoLectureList />} />
                     <Route path="/lectures/:id" element={<VideoLectureDetail />} />
-                    <Route path="/about" element={<About />} />
+
                 </Routes>
                 <Footer />
             </Router>
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,29 +1,31 @@
-import { Container, Typography } from '@mui/material';
-import { makeStyles } from '@mui/styles';
+// src/AboutPage.js
 
-const useStyles = makeStyles({
-    root: {
-        marginTop: '20px',
-        marginBottom: '20px',
-    },
-});
+import React from 'react';
+import { Box, Typography } from '@mui/material';
 
-const About = () => {
-    const classes = useStyles();
-
+const AboutPage = () => {
     return (
-        <Container className={classes.root}>
-            <Typography variant="h4" gutterBottom>
-                About Us
+        <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" p={4}>
+            <Typography variant="h2" mb={2}>
+                부동산융합아카데미란?
             </Typography>
             <Typography variant="body1" paragraph>
-                Welcome to Video Lecture Site, the best place to learn anything online. We are a team of dedicated educators and developers working together to create an accessible platform for everyone.
+                부동산융합아카데미는 미래 세대를 위한 진보적인 교육 플랫폼입니다.
+                우리는 부동산와 관련된 가장 효과적인 교육 방법론을 제공하며,
+                학생들이 미래의 경제에서 성공적으로 활동할 수 있도록 돕습니다.
             </Typography>
             <Typography variant="body1" paragraph>
-                Our mission is to make learning easy and fun. We believe that everyone has the right to education, and we strive to provide the best quality lectures from experts all around the world.
+                부동산융합아카데미에서는 학생들에게 AI를 이해하고 이를 사용하여 문제를 해결하는 능력을
+                키울 수 있는 기회를 제공합니다.
+                학생들은 최신 기술 동향을 이해하고 이를 이용하여 실제 생활에서의 문제를 해결하는데 필요한
+                핵심적인 능력을 배울 수 있습니다.
             </Typography>
-        </Container>
+            <Typography variant="body1" paragraph>
+                우리의 핵심 가치는 창의적 사고, 혁신, 그리고 연속적인 학습입니다.
+                우리는 학생들이 이러한 가치를 실현하도록 돕는데 전념하고 있습니다.
+            </Typography>
+        </Box>
     );
 }
 
-export default About;
+export default AboutPage;
